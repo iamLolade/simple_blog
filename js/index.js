b40b636bc8ab6fedf282127c416c47fb97c5f99a
iamLolade <adeniyiotemade@gmail.com>
2021-12-22 14:57:38 +0100

    Fetched all data from endpoint

---
+++ js/index.js
@@ -0,0 +1,32 @@
+//Container
+const container = document.querySelector(".blogs")
+
+//Render posts
+const renderPosts = async () => {
+    let uri = "http://localhost:3000/posts";
+
+    const res = await fetch(uri);
+    const posts = await res.json();
+
+    console.log(posts)
+
+    let template = "";
+    posts.forEach(post => {
+        template += `
+            <div class="post">
+                <h2>${post.title}</h2>
+                <p><small>${post.likes} ❤️</small></p>
+                <p>${post.body.slice(0, 200)}</p>
+                <a href="/details.html">Read more...</a>
+            </div>
+        `
+    });
+
+    container.innerHTML = template;
+}
+
+
+
+
+
+window.addEventListener("DOMContentLoaded", () => renderPosts());
